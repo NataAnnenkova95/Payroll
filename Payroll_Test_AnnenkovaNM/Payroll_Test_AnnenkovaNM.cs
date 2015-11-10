6392fb87b902659d5aa3b2ed16f0f769d925a750
NataAnnenkova95 <nata.annenkova.95@mail.ru>
2015-11-10 09:42:41 +0300

    Создан Employee и тесты

---
+++ Payroll_Test_AnnenkovaNM/Payroll_Test_AnnenkovaNM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payroll_AnnenkovaNM;
+
+namespace Payroll_Test_AnnenkovaNM
+{
+    [TestClass]
+    public class Payroll_Test_AnnenkovaNM
+    {
+        [TestMethod]
+        public void TestEmployee()
+        {
+            int empid = 1;
+            Employee e = new Employee(empid, "Natasha", "Slancy");
+            Assert.AreEqual("Natasha", e.Name);
+            Assert.AreEqual("Slancy", e.Address);
+            Assert.AreEqual(empid, e.EmpId);
+
+        }
+    }
+}
---
+++ Payroll_Test_AnnenkovaNM/TestEmployeeToString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payroll_AnnenkovaNM;
+
+namespace Payroll_Test_AnnenkovaNM
+{
+    [TestClass]
+    public class TestEmployeeToString
+    {
+        [TestMethod]
+        public void EmployeeToString()
+        {
+            int empid = 1;
+            Employee e = new Employee(empid, "Natasha", "Slancy");
+            Assert.AreEqual("Emp#: 1 Natasha Slancy Paid by ",e.ToString());
+        }
+    }
+}
